d4c7c94aead337137528f865c17c4b0d173b6cc9
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-30 16:37:43 -0600

    add modular button for adding and returning back to merch

--- src/components/MerchControl.js
+++ src/components/MerchControl.js
@@ -6,18 +6,37 @@ class MerchControl extends React.Component {
 
   constructor(props) {
     super(props);
-      this.state = {
-        formIsVisibleOnPage: false
+    this.state = {
+      formVisibleOnPage: false
+    };
   }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewMerchForm />
+      buttonText = "Return to Merch"
+    } else {
+      currentlyVisibleState = <MerchList />
+      buttonText = "Add Merch";
+    }
     return (
-      <>
-      </>
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
     );
   }
+
 }
+
 
 export default MerchControl;
 
