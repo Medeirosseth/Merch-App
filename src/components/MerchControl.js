ff4f45ef3d0d5db7c2880bc8d115daa3a7e7e980
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-06-30 16:42:00 -0600

    add bound handler click

--- src/components/MerchControl.js
+++ src/components/MerchControl.js
@@ -9,6 +9,7 @@ class MerchControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = () => {
